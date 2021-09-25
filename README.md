d0079768f8f65fecfc3cf2ef6bd87ea00bfc66d9
William Cortes <williamcortesro@outlook.es>
2021-09-24 20:31:13 -0500

    index.html: cambio de estilos de bootstrap a css

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # theBeatles
+
+Esta aplicación fue realizada aprendiendo Node.js
+
+Puedes ver los cuatro miembros de la banda The Beatles, 👨‍🎤
+
+y tambien tienes acceso para realizar un request si deseas practicar 💪.
+
+puedes verla en:   
+
+https://thebeatlesapi.herokuapp.com/
--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var http = require('http');
 var fs   = require('fs');
 require('dotenv').config();
 
-
 var beatles=[{
   name: "John Lennon",
   birthdate: "09/10/1940",
@@ -24,33 +23,10 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
 const beatlesNames =["John%20Lennon","Paul%20McCartney","George%20Harrison","Richard%20Starkey" ]
 
-// const handleServer = (req, res) =>{
-//       let i = 0
-//       for(let j=0; j<beatlesNames.length; j++){
-//             if(req.url ==="/api/"+beatlesNames[j]){
-//                   i = j
-//               }
-//          }
-//          if(req.url === "/"){
-//             res.writeHead(200, {'Content-type': 'text/html'})
-//         		var html = fs.readFileSync(__dirname +'/index.html');
-//             res.end(html)
 
-//         } else if ( req.url === "/api") {
-//           res.writeHead(200, { 'Content-type': 'application/json' })
-//           //var text = fs.readFileSync(__dirname + beatles );   
-//           res.end(JSON.stringify(beatles))
-//       } else if( req.url === "/api/" +beatlesNames[i] ){
-//         res.writeHead(200, { 'Content-type': 'application/json' })
-//         res.end(JSON.stringify(beatles[i]))
-//       }else{
-//           res.writeHead(200, { 'Content-type': 'text/html' })
-//           res.write('<p>Error, Creado por Will I am ♥</p>') 
-//           res.end()
-//       }
-// }
 const handleServer = (req, res) =>{
       let i = 0
       for(let j=0; j<beatlesNames.length; j++){
@@ -66,7 +42,7 @@ const handleServer = (req, res) =>{
       switch(req.url){
         case "/":
             res.writeHead(200, {'Content-type': 'text/html'})
-        		var html = fs.readFileSync(__dirname +'/index.html');
+        		var html = fs.readFileSync(__dirname +'/views/index.html');
             res.end(html)
             break
         case "/api":
@@ -79,7 +55,7 @@ const handleServer = (req, res) =>{
             break
         case "/"+beatlesNames[i]:
             res.writeHead(200, { 'Content-Type':'text/html' })
-            var html = fs.readFileSync(__dirname +'/beatle.html', 'utf8'); //Codificamos el buffer para que sea una String
+            var html = fs.readFileSync(__dirname +'/views/beatle.html', 'utf8'); //Codificamos el buffer para que sea una String
             var nombre = beatles[i].name; //Esta es la variable con la que vamos a reemplazar el template
             var description = `Birthdate: ${beatles[i].birthdate}`;
             var img =`<img src=${beatles[i].profilePic}>`;
@@ -89,66 +65,16 @@ const handleServer = (req, res) =>{
         default:
             res.writeHead(200, { 'Content-type': 'text/html' })
             // res.write('<p>Error, Creado por Will I am ♥</p>') 
-            var html = fs.readFileSync(__dirname +'/nofound.html', 'utf8'); //Codificamos el buffer para que sea una String
+            var html = fs.readFileSync(__dirname +'/views/nofound.html', 'utf8'); //Codificamos el buffer para que sea una String
             res.end(html)
       }
 }
 
-const martina = (req, res) =>{
-  console.log('lo que viene en req.url: ',req.url)
-  if(req.url === '/api'|| req.url === '/api/'){
-    res.writeHead(200, { 'Content-type': 'application/json' })
-    res.end(JSON.stringify(beatles))
-  }
-  if(req.url.substring(0,5) ==='/api/' && req.url.length > 5){
-    let findBeatle = req.url.split('/').pop()
-    let foundBeatle = beatles.find( beatle => {
-      return findBeatle === encodeURI(beatle.name)//encodeUrl me pone %20 en el nombre
-    })
-    if(foundBeatle){
-      res.writeHead(200, {'Content-type': 'application/json'})
-    res.end(JSON.stringify(foundBeatle))
-    } else {
-      res.writeHead(404, {'Content-type': 'text/plain'})
-      res.end('El beatle no existe')
-    }
-  }
-  if(req.url === '/'){
-    res.writeHead(200, {'Content-type': 'text/html'})
-    const index = fs.readFileSync(`${__dirname}/index.html`); //Codificamos el buffer para que sea una String
-    res.end(index) 
-  }
-  let findBeatle = req.url.split('/').pop()
-  let foundBeatle = beatles.find( beatle => {
-    return findBeatle === encodeURI(beatle.name)//encodeUrl me pone %20 en el nombre
-  })
-  if(foundBeatle){
-    res.writeHead(200, {'Content-type': 'text/html'})
-    const b = fs.readFileSync(`${__dirname}/beatle.html`, 'utf-8')
-    console.log('el betlefound es: ',b)
-    let finalHTML = replaceData(b, foundBeatle);
-    res.end(finalHTML);
-  }else{
-    res.writeHead(404, {'Content-Type':'text/plain'});
-    res.end('No existe ese Beatle');
-  }
-}
 
-
-function replaceData(template, beatle){
-  return template
-      .replace('{name}', beatle.name)
-      .replace('{name_name}', beatle.name)
-      .replace('{birth}', beatle.birthdate)
-      .replace('{profilePic}', beatle.profilePic);
-}
 const server = http.createServer(handleServer)
-//const server = http.createServer(martina)
 const port_number = process.env.PORT || 1338;
 const host = '0.0.0.0';
-
 
 server.listen(port_number, host,() =>{
   console.log(`Server listenning on port: ${port_number}`)
 })
-// server.listen(1338, '127.0.0.1')
